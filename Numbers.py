14ee5165d51d8aed3be1b0856f58ae53d9b35172
Angel Angelov <angelangelov_1980@abv.bg>
2018-10-28 00:30:59 +0300

    moving to git

---
+++ Numbers.py
@@ -0,0 +1,16 @@
+from math import *
+
+print(2.0987)
+
+my_num = -5
+print(my_num)
+print(str(my_num) + " my favorite number")
+print(abs(my_num))
+print(pow(4, 6))
+print(max(4, 6))
+print(min(4, 6))
+print(round(3.2))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(3.7))
+
