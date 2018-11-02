752be29ab2a3ce6e53d013aa782afe9f0701e079
Angel Angelov <angelangelov_1980@abv.bg>
2018-11-02 23:34:51 +0200

    moving to git

---
+++ List.py
@@ -0,0 +1,13 @@
+
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+friends = ['Kevin', "Karen", 'Jim', "Oscar", "Toby"]
+
+print(friends[1:4])
+friends.insert(1, "Kelly")
+friends.extend(lucky_numbers)
+friends.append("Creed")
+friends.pop()
+print(friends)
+
+
+
