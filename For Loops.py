a8ded4d6c3d9ab36c81158fc3bd67d2e601f5368
Angel Angelov <32064940+AngelVAngelov@users.noreply.github.com>
2018-11-23 08:48:33 +0200

    update

---
+++ For Loops.py
@@ -0,0 +1,21 @@
+
+
+for letter in "Giraffe Academy":
+    print(letter)
+
+friends = ["jim", "Karen", "Kevin"]
+for friend in friends:
+    print(friend)
+
+for index in range(3, 10):
+    print(index)
+
+friends = ["jim", "Karen", "Kevin"]
+for index in range(len(friends)):
+    print(friends[index])
+
+for index in range(5):
+    if index == 0:
+        print("First Iteration")
+    else:
+        print("Not first")
